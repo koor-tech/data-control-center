f457683db57189433e1498a52c4fa914477b8e23
Dave Mount <dave@koor.tech>
2023-08-10 15:17:38 -0700

    fix: add history fallback; try to get env vars

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const history = require('connect-history-api-fallback')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(express.static('dist/spa'))
+app.use(history())
 
 app.get('/ping', (req, res) => {
   res.send('pong')
--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = ENV.SUPABASE_URL || process.env.SUPABASE_URL
+const supabaseAnonKey = ENV.SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
