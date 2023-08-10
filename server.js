f62256d93b2f3932dcaed4ff794b9276223a5861
Dave Mount <dave@koor.tech>
2023-08-10 14:44:09 -0700

    fix: go back to commonjs project type

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import 'dotenv/config'
+const express = require('express')
+const dotenv = require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT
