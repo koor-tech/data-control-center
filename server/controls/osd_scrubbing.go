305b37c8bb0042af9005c402963b055418bacf44
Alexander Trost <galexrt@googlemail.com>
2024-01-15 23:59:03 +0100

    feat: add set osd scrubbing schedule config logic
    Only the UI is needed to send the right values now.
    
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- server/controls/osd_scrubbing.go
+++ server/controls/osd_scrubbing.go
@@ -12,12 +12,12 @@ import (
 )
 
 func (s *Server) GetScrubbingSchedule(ctx context.Context, req *connect.Request[pb.GetScrubbingScheduleRequest]) (*connect.Response[pb.GetScrubbingScheduleResponse], error) {
-
 	list, err := s.k.GetRookClient().CephV1().CephClusters(s.Namespace).List(ctx, v1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
 
+	// There can only be one CephCluster per namespace
 	var cluster rookcephv1.CephCluster
 	for _, c := range list.Items {
 		cluster = c
@@ -91,17 +91,6 @@ func (s *Server) GetScrubbingSchedule(ctx context.Context, req *connect.Request[
 		schedule.EndWeekDay = &value
 	}
 
-	// osd_scrub_end_week_day
-	if val, ok := globalCfg["osd_scrub_end_week_day"]; ok {
-		parsed, err := strconv.Atoi(val)
-		if err != nil {
-			return nil, err
-		}
-
-		value := int64(parsed)
-		schedule.EndWeekDay = &value
-	}
-
 	// osd_scrub_min_interval
 	if val, ok := globalCfg["osd_scrub_min_interval"]; ok {
 		parsed, err := strconv.Atoi(val)
@@ -155,7 +144,37 @@ func (s *Server) SetScrubbingSchedule(ctx context.Context, req *connect.Request[
 	schedule := req.Msg.OsdScrubbingSchedule
 	schedule.Default()
 
-	// TODO
+	list, err := s.k.GetRookClient().CephV1().CephClusters(s.Namespace).List(ctx, v1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	// There can only be one CephCluster per namespace
+	var cluster rookcephv1.CephCluster
+	for _, c := range list.Items {
+		cluster = c
+		break
+	}
+
+	cephConfig := cluster.Spec.CephConfig
+	if _, ok := cephConfig["global"]; !ok {
+		cephConfig["global"] = map[string]string{}
+	}
+
+	globalCfg := cephConfig["global"]
+	globalCfg["osd_max_scrubs"] = strconv.Itoa(int(*schedule.MaxScrubOps))
+	globalCfg["osd_scrub_begin_hour"] = strconv.Itoa(int(*schedule.BeginHour))
+	globalCfg["osd_scrub_end_hour"] = strconv.Itoa(int(*schedule.EndHour))
+	globalCfg["osd_scrub_begin_week_day"] = strconv.Itoa(int(*schedule.BeginWeekDay))
+	globalCfg["osd_scrub_end_week_day"] = strconv.Itoa(int(*schedule.EndWeekDay))
+	globalCfg["osd_scrub_min_interval"] = strconv.Itoa(int(*schedule.MinScrubInterval))
+	globalCfg["osd_scrub_max_interval"] = strconv.Itoa(int(*schedule.MaxScrubInterval))
+	globalCfg["osd_deep_scrub_interval"] = strconv.Itoa(int(*schedule.DeepScrubInterval))
+	globalCfg["osd_scrub_sleep"] = strconv.Itoa(int(*schedule.ScrubSleepSeconds))
+
+	if _, err := s.k.GetRookClient().CephV1().CephClusters(s.Namespace).Update(ctx, &cluster, v1.UpdateOptions{}); err != nil {
+		return nil, err
+	}
 
 	return connect.NewResponse(&pb.SetScrubbingScheduleResponse{}), nil
 }
