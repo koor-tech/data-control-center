e8cbd6f50a6df14c1b73df5981dfc4cdd46d4990
Alexander Trost <galexrt@googlemail.com>
2024-02-05 16:46:03 +0100

    feat: add basic erasure coded pool size check
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- pkg/recommender/modules/pool_sizes.go
+++ pkg/recommender/modules/pool_sizes.go
@@ -33,16 +33,29 @@ func (m *PoolSizes) Run(ctx context.Context, p *Params) ([]*cephv1.ClusterRecomm
 		return nil, err
 	}
 	for _, bp := range blockPools.Items {
-		if bp.Spec.IsErasureCoded() {
-			// TODO
-		} else if bp.Spec.IsReplicated() {
-			// TODO
-			if bp.Spec.Replicated.Size < 3 {
-				poolName := bp.Spec.Name
-				if poolName == "" {
-					poolName = bp.GetName()
-				}
+		poolName := bp.Spec.Name
+		if poolName == "" {
+			poolName = bp.GetName()
+		}
 
+		if bp.Spec.IsErasureCoded() {
+			if bp.Spec.ErasureCoded.CodingChunks < 2 || bp.Spec.ErasureCoded.DataChunks < 2 {
+				recs = append(recs, &cephv1.ClusterRecommendation{
+					Title:       fmt.Sprintf("Pool - %s: Erasure coded chunks not recommended", poolName),
+					Description: "Erasure coded chunk coding/data chunks sizes not recommended, please see https://docs.ceph.com/en/latest/rados/operations/erasure-code/#erasure-code-profiles.",
+					Level:       cephv1.RecommendationLevel_RECOMMENDATION_LEVEL_HIGH,
+					Type:        cephv1.RecommendationType_RECOMMENDATION_TYPE_POOL,
+					ExtraData: &cephv1.ClusterRecommendation_RecommendedValue{
+						RecommendedValue: &cephv1.RecommendedValue{
+							Current:  fmt.Sprintf("Coding chunks: %d, Data chunks: %d", bp.Spec.ErasureCoded.CodingChunks, bp.Spec.ErasureCoded.DataChunks),
+							Expected: "> 2",
+						},
+					},
+				})
+			}
+
+		} else if bp.Spec.IsReplicated() {
+			if bp.Spec.Replicated.Size < 3 {
 				recs = append(recs, &cephv1.ClusterRecommendation{
 					Title:       fmt.Sprintf("Pool - %s: Replicated size less than 3", poolName),
 					Description: "Pool replicated size should be 3 or higher in production environments.",
@@ -57,7 +70,7 @@ func (m *PoolSizes) Run(ctx context.Context, p *Params) ([]*cephv1.ClusterRecomm
 				})
 			}
 		} else if bp.Spec.IsHybridStoragePool() {
-			// TODO
+			// TODO what to check for a hybrid storage pool
 		}
 	}
 
